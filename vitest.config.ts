992befd5bc9296f68e532f80268b9242f241d104
vishesh <visheshbaghel99@gmail.com>
2025-02-26 00:34:43 +0530

    added initial code for operations

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    workspace: ['packages/*', 'deployers/*', 'speech/*'],
+    workspace: [
+      'packages/*',
+      'vector-stores/*',
+      'deployers/*',
+      'speech/*',
+      '!vector-stores/docker-compose.yaml',
+      '!vector-stores/**/*.md',
+    ],
   },
 });
